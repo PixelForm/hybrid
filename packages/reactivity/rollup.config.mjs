7dd00b89d9c1e37bf93d09fdb38824689bf53e32
PixelForm <leanderdemooij2000@gmail.com>
2024-05-30 09:25:53 +0200

    Modified rollup config for npm package and typescript

---
+++ packages/reactivity/rollup.config.mjs
@@ -0,0 +1,21 @@
+import typescript from '@rollup/plugin-typescript'
+import { dts } from 'rollup-plugin-dts'
+import { readFile } from 'node:fs/promises'
+
+const pkg = JSON.parse(await readFile('package.json'))
+
+export default [
+	{
+		input: 'src/index.ts',
+		output: [
+			{ file: pkg.main, format: 'cjs', sourcemap: true },
+			{ file: pkg.module, format: 'es', sourcemap: true },
+		],
+        plugins: [typescript()]
+    },
+    {
+        input: 'src/index.d.ts',
+        output: { file: pkg.types, format: 'es' },
+        plugins: [dts()]
+    }
+]
