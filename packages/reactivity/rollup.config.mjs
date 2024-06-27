0392e64758ef33c6dd3921aab6db73afbb0c01ba
PixelForm <leanderdemooij2000@gmail.com>
2024-06-27 20:51:51 +0200

    Modified build config to include better modules

--- packages/reactivity/rollup.config.mjs
+++ packages/reactivity/rollup.config.mjs
@@ -1,13 +1,26 @@
 import typescript from '@rollup/plugin-typescript'
+import terser from '@rollup/plugin-terser'
 import { readFile } from 'node:fs/promises'
 
 const pkg = JSON.parse(await readFile('package.json'))
+
+const minify = () => terser({
+    compress: {
+        ecma: 2022,
+        unsafe_arrows: true,
+        inline: true,
+        keep_fargs: false,
+        module: true,
+        passes: 3
+    }
+})
 
 export default [
     {
         input: 'src/index.ts',
         output: [
-            { file: pkg.main, format: 'cjs' },
+            { file: pkg.main, format: 'umd', name: 'Hyper' },
+            { file: pkg.unpkg, format: 'umd', name: 'Hyper', plugins: [minify()] },
             { file: pkg.module, format: 'es' },
         ],
         plugins: [typescript()],
