d1b9077fbe6aded9b39a0485baff9776adeea683
PixelForm <leanderdemooij2000@gmail.com>
2024-06-28 22:01:44 +0200

    Added terser for minification of unpkg build

--- packages/reactivity/rollup.config.mjs
+++ packages/reactivity/rollup.config.mjs
@@ -4,16 +4,17 @@ import { readFile } from 'node:fs/promises'
 
 const pkg = JSON.parse(await readFile('package.json'))
 
-const minify = () => terser({
-    compress: {
-        ecma: 2022,
-        unsafe_arrows: true,
-        inline: true,
-        keep_fargs: false,
-        module: true,
-        passes: 3
-    }
-})
+const minify = () =>
+    terser({
+        compress: {
+            ecma: 2022,
+            unsafe_arrows: true,
+            inline: true,
+            keep_fargs: false,
+            module: true,
+            passes: 3,
+        },
+    })
 
 export default [
     {
